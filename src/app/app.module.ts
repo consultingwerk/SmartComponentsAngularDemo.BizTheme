46a473255e6261d8ac1a88b2ced669f3edd82ad0
Radu Nicoara <nicoara.radu16@gmail.com>
2018-12-06 12:32:00 +0200

    updated to NG7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomRootComponent } from './custom-root/custom-root.component';
 import { LogoutComponent } from './logout/logout.component';
 
-import { load, IntlModule } from '@progress/kendo-angular-intl';
-import * as likelySubtags from 'cldr-data/supplemental/likelySubtags.json';
-import * as  weekData from 'cldr-data/supplemental/weekData.json';
-import * as currencyData from 'cldr-data/supplemental/currencyData.json';
-import * as numbers from 'cldr-data/main/de/numbers.json';
-import * as timeZoneNames from 'cldr-data/main/de/timeZoneNames.json';
-import * as calendar from 'cldr-data/main/de/ca-gregorian.json';
-import * as currencies from 'cldr-data/main/de/currencies.json';
-import * as dateFields from 'cldr-data/main/de/dateFields.json';
+import { IntlModule } from '@progress/kendo-angular-intl';
 import { CustomerMaintenanceFormModule } from './forms/customer-maintenance/customer-maintenance.form';
 import { DeveloperToolsCustomFormModule } from './forms/developer-tools-custom/developer-tools-custom.form';
 import { StartPageModule } from './start-page/chart-template.component';
@@ -26,17 +18,7 @@ import {CustomerTabfolderFormModule} from "./forms/customer-tabfolder/customer-t
 import {CustomerMaintenanceHtmlFormModule} from "./forms/customer-maintenance-html/customer-maintenance-html.form";
 import { CustomerMaintenanceAnnotationsFormModule } from './forms/customer-maintenance-annotations/customer-maintenance-annotations.form';
 import { environment } from '../environments/environment';
-
-load(
-  likelySubtags,
-  weekData,
-  currencyData,
-  numbers,
-  currencies,
-  calendar,
-  dateFields,
-  timeZoneNames
-);
+import '@progress/kendo-angular-intl/locales/de/all';
 
 @NgModule({
   declarations: [
